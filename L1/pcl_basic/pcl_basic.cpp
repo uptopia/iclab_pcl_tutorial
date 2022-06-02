8c278e6cdc6f1b1744d265bcb4cf25ccbe4fcdfc
uptopia <charlotte07221@gmail.com>
2022-06-02 22:31:59 +0800

    update pcl_basic

--- L1/pcl_basic/pcl_basic.cpp
+++ L1/pcl_basic/pcl_basic.cpp
@@ -7,17 +7,17 @@
 #include<pcl/filters/filter.h> //removeNan
 using namespace std;
 
-pcl::PointXYZ PointT;
+typedef pcl::PointXYZ PointT;
 
 int main()
 {
     //===========//
     // Parameters
     //===========//
-    file_load_path_cloud1 = "";
-    file_load_path_cloud2 = "";
-    file_save_path = "";
-    file_save_pathASCII = "";
+    std::string file_load_path_cloud1 = "/home/upup/iclab_pcl_tutorial/example_data/scene_D405_cut.pcd";
+    std::string file_load_path_cloud2 = "/home/upup/iclab_pcl_tutorial/example_data/scene_D435i.pcd";
+    std::string file_save_path = "/home/upup/iclab_pcl_tutorial/example_data/cloud_binary.pcd";
+    std::string file_save_pathASCII = "/home/upup/iclab_pcl_tutorial/example_data/cloud_ascii.pcd";
      
     pcl::PointCloud<PointT>::Ptr cloud1(new pcl::PointCloud<PointT>);
     pcl::PointCloud<PointT>::Ptr cloud2(new pcl::PointCloud<PointT>);
@@ -27,13 +27,13 @@ int main()
     //===========//
     if(pcl::io::loadPCDFile<PointT>(file_load_path_cloud1, *cloud1)== -1)
     {
-        PCL_ERROR("Failed to load file" + file_load_path_cloud1);
+        PCL_ERROR("Failed to load file %s", file_load_path_cloud1);
         system("pause");
         return -1;
     }
     if(pcl::io::loadPCDFile<PointT>(file_load_path_cloud2, *cloud2)== -1)
     {
-        PCL_ERROR("Failed to load file" + file_load_path_cloud2);
+        PCL_ERROR("Failed to load file %s", file_load_path_cloud2);
         system("pause");
         return -1;
     }
@@ -44,7 +44,7 @@ int main()
     //https://pointclouds.org/documentation/tutorials/basic_structures.html
     int width = cloud1->width;
     int height = cloud1->height;
-    int size = cloud1->size;
+    int size = cloud1->size();
     bool is_dense = cloud1->is_dense;    //True: No NaN/Inf, all points are finit
     Eigen::Vector4f ori_translation = cloud1->sensor_origin_;
     Eigen::Quaternionf ori_orientation = cloud1->sensor_orientation_;
@@ -57,7 +57,9 @@ int main()
     //(1)pcl_isfinite
     for(int i =0; i<cloud1->size(); i++)
     {
-        if(!pcl_isfinite(cloud1->at(i)))//skipping Nans
+        PointT pt = cloud1->points[i];
+        //skipping Nans
+        if(!pcl_isfinite(pt.x) || !pcl_isfinite(pt.y) || !pcl_isfinite(pt.z))
         {
             continue;
         }
@@ -65,7 +67,7 @@ int main()
     //(2)removeNaNFromPointCloud
     cloud1->is_dense = false;
     std::vector<int> nan_indices;
-    pcl::removeNaNFrom PointCloud(*cloud1, *cloud1, nan_indices);
+    pcl::removeNaNFromPointCloud(*cloud1, *cloud1, nan_indices);
 
     //===================//
     // Center & Centroid
@@ -97,40 +99,34 @@ int main()
     //===============//
     // Visualization
     //===============//
-    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("add 2 clouds"));
     viewer->initCameraParameters();
-    int v1(0),v2(0);;
+    int v1(0),v2(0);
 
-    viewer->createViewPort(0.0,0.0,0.5,1.0,v1);//(Xmin,Ymin,Xmax,Ymax)設置窗口座標
-    viewer->setBackgroundColor(0,0,0,v1);//設置背景
-    viewer->addText("original clouds", 10,10,"v1 text", v1);//設置視口名稱
+    // viewport 1: ori + voxel
+    viewer->createViewPort(0.0,0.0,0.5,1.0,v1); //(Xmin,Ymin,Xmax,Ymax)設置窗口座標
+    viewer->setBackgroundColor(0,0,0,v1);       //設置背景
+    viewer->addText("original clouds (wh); added cloud (rd)", 10,10,"v1 text", v1);
+    viewer->addPointCloud<PointT>(cloud2, pcl::visualization::PointCloudColorHandlerCustom<PointT>(cloud2, 255, 255, 255), "cloud2", v1);
+    viewer->addPointCloud<PointT>(cloud1, pcl::visualization::PointCloudColorHandlerCustom<PointT>(cloud1, 255, 0, 0), "cloud1", v1);
+    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE,2,"cloud1");
 
-    pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud1_color(cloud1, 0, 255, 0); // green
-    pcl::visualization::PointCloudColorHandlerCustom<PointT> cloud2_color(cloud2, 255, 0, 0); // green
-    
-    viewer->addPointCloud<pcl::PointXYZ>(cloud1, cloud1_color, "cloud1", v1);//添加點雲
-    viewer->addPointCloud<pcl::PointXYZ>(cloud2, cloud2_color, "cloud2", v1);//添加點雲
-	
+    //viewport 2: voxel
     viewer->createViewPort(0.5,0.0,1.0,1.0,v2);
     viewer->setBackgroundColor(0.3,0.3,0.3,v2);
-    viewer->addText("combined cloud", 10,10,"v2 text", v2);
-    viewer->addPointCloud(cloud_combined,"cloud_combined", v2);
+    viewer->addText("added cloud, total pts = " + to_string(cloud1->size()), 10,10,"cloud1 text", v2);
+    viewer->addPointCloud(cloud1,"cloud_filtered_only", v2);
 
-    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_FONT_SIZE,3,"cloud1");
-    viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_FONT_SIZE,3,"cloud2");
     while (!viewer->wasStopped ())
     {
-
-     viewer->spinOnce();
+        viewer->spinOnce();
     }
-
 
     //===========//
     // Save PCD
     //===========//
-    pcl::savePCDFile(file_save_path, *cloud);
-    pcl::savePCDFileASCII(file_save_path, *cloud);
-    
+    pcl::io::savePCDFileBinary(file_save_path, *cloud2);
+    pcl::io::savePCDFileASCII(file_save_pathASCII, *cloud2);
     
     return 0;
 }
